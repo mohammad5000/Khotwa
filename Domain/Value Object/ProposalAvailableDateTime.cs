284093e4a4210c8ff551d147e2c2b2320224b11d
mohammad5000 <mohammadashraf5000@gmail.com>
2025-07-07 11:31:19 +0300

    added value object ProposalAvailableDateTime

---
+++ Domain/Value Object/ProposalAvailableDateTime.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class ProposalAvailableDate
+{
+    [Required]
+    public DateTime AvailableDateTime { get; set; }
+
+    [Required]
+    [ForeignKey("Proposal")]
+    public int ProposalId { get; set; }
+
+    public Proposal? Proposal { get; set; }
+}
